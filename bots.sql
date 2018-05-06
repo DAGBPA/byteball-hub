109781f4665e6d09d7bf25e634f72c2c54b257b7
guantau <alphatau@163.com>
2018-05-06 11:36:38 +0800

    update bots

--- bots.sql
+++ bots.sql
@@ -5,6 +5,6 @@ INSERT INTO bots (name, description, pairing_code) VALUES (
 );
 INSERT INTO bots (name, description, pairing_code) VALUES (
 	'BPA oracle', 
-	'This oracle posts Merkle Roots of all BitcoinPizza transactions in a block every time a new BitcoinPizza block is mined. You can use its data to P2P trade Bytes vs BTC. If you are receiving Bytes (sending bitcoins), chat with the oracle after sending your bitcoins to get the Merkle Proof of your Bitcoin transaction and unlock your bytes from the smart contract.', 
+	'This oracle posts Merkle Roots of all BitcoinPizza transactions in a block every time a new BitcoinPizza block is mined. You can use its data to P2P trade Pizza vs BPA. If you are receiving Pizza (sending bitcoins), chat with the oracle after sending your bitcoins to get the Merkle Proof of your Bitcoin transaction and unlock your pizza from the smart contract.', 
 	'A7C96Bhg4Gpb2Upw/Ky/YfGG8BKe5DjTiBuJFGAX50N1@dagpizza.org/bb#0000'
 );
